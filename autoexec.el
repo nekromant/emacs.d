5f2724ce58f2a23402e88199d8ed46667ebfff77
Andrew 'Necromant' Andrianov <necromant@necromant.ath.cx>
2012-05-25 00:05:22 +0400

    Autocommit from emacs

--- autoexec.el
+++ autoexec.el
@@ -34,57 +34,13 @@
                 (c-set-style "linux-tabs-only")))
 )
 
-(defun th-c-header(name)
-  (interactive "sHeader name: ")
-  (insert (concat "#ifndef __" name "_H\n"))
-  (insert (concat "#define __" name "_H\n\n"))
-  (insert "#endif\n")
-  (forward-line -2)
-)
-
-
-;---
-
-(setq th-ffk-path '"~")
-(defun th-find-file-kio ()
-  (interactive)
-  (let ((file-name
-         (replace-regexp-in-string
-          "[\n]+" ""
-          (shell-command-to-string (concat "kdialog --getopenurl " th-ffk-path " 2> /dev/null")))))
-    (message file-name)
-    (cond
-     ((string-match "^file://" file-name)
-      ;; Work arround a bug in kioexec, which causes it to delete local
-      ;; files. (See bugs.kde.org, Bug 127894.) Because of this we open the
-      ;; file with `find-file' instead of emacsclient.
-      (let ((local-file-name (substring file-name 7)))
-        (message "Opening local file '%s'" local-file-name)
-	(setq th-ffk-path local-file-name )
-        (find-file local-file-name)))
-     ((string-match "^[:space:]*$" file-name)
-      (message "Empty file name given, doing nothing..."))
-     (t
-      (message "Opening remote file '%s'" file-name)
-      (save-window-excursion
-        (shell-command (concat "kioexec emacsclient " file-name "&"))))))
-  )
-
-(defun th-emacs-push()
-  (interactive)
-  (shell-command (concat "cd ~/.emacs.d; git commit -a -m \"Autocommit from emacs\"; git push"))
-  )
-
-(defun th-emacs-pull()
-  (interactive)
-  (shell-command "cd ~/.emacs.d; git pull --recurse-submodules=on;")
-  )
-
-(defun th-prof (prof)
-  (interactive "sProfile, plz: ")
-  (load-file (concat "~/.emacs.d/profiles/" prof ".el"))
-  )
-
+(add-to-list 'load-path "~/.emacs.d/doxymacs/lisp")
+(require 'doxymacs)
+(add-hook 'c-mode-common-hook'doxymacs-mode)
+(defun my-doxymacs-font-lock-hook ()
+    (if (or (eq major-mode 'c-mode) (eq major-mode 'c++-mode))
+        (doxymacs-font-lock)))
+  (add-hook 'font-lock-mode-hook 'my-doxymacs-font-lock-hook)
 
 (defun th-reload()
   (interactive)
